2b01576bdf1b6719e9f18b6dd2e02f419f2dcca3
Cristian <cristian.valenciaa@alumnos.uv.cl>
2020-11-15 19:48:29 -0300

    Validador DesarrolloWeb

---
+++ src/NuevosComponentes/MiComponente.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+
+class MiComponente extends Component {
+    render() {
+        return ( 
+        <div>
+            <img src = "https://memeschistosos.net/wp-content/uploads/2015/10/memesdefelicitaciones1.jpg" />
+        </div>
+        );
+
+    }
+}
+
+export default MiComponente;
